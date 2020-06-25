daba1c02e9525d3bc47e3c100b3a668e0b24aa55
Vatsal Bajaj <53997634+bajajvatsal@users.noreply.github.com>
2020-06-25 10:55:30 +0530

    Queue algorithm added

---
+++ queue_algorithms.py
@@ -0,0 +1,72 @@
+import sys
+from list_creation import unordered_array  # list of numbers of desired length randomly ordered no repetition
+
+
+class QueueOverflow(BaseException):
+    pass
+
+
+class QueueUnderflow(BaseException):
+    pass
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = unordered_array(length_array)
+        self.limit = limit_array
+        self.length = len(self.queue)
+        self.front = self.length - 1
+        self.rear = 0
+
+    def is_full(self):
+        if self.front >= self.limit:
+            print("Queue Overflow")
+            raise QueueOverflow
+        else:
+            print("Space available for operations :)")
+
+    def is_empty(self):
+        if self.length == self.rear:
+            print("Queue Underflow! No operations can be done")
+            raise QueueUnderflow
+        else:
+            print("Space available for operations")
+
+    def enqueue(self, val):
+        if self.front == self.limit:
+            print("Queue Overflow! No operations can be done")
+        else:
+            self.queue.insert(0, val)
+            print(f"Enqueue : {self.queue}")
+
+    def dequeue(self):
+        if self.front == self.rear:
+            print("Queue Underflow! No operations can be done")
+        else:
+            print(f"Element to be popped out is: {self.queue[self.front]}")
+            self.queue.pop(self.length - 1)
+            print(f"Dequeue : {self.queue}")
+
+
+if __name__ == '__main__':
+    length_array = int(input("Length of queue: "))
+    limit_array = int(input("Limit of queue: "))
+    if length_array == limit_array:
+        print("No space available operations cant be done. Please re-run the program")
+        sys.exit()
+    elif length_array > limit_array:
+        print("Length of queue must be lesser than the limit . Please re-run the program")
+        sys.exit()
+    operation = input("which operation to do: (f)if_full, (m)is_empty, (e)enqueue, (d)dequeue: ")
+    q = Queue()
+    if operation == 'f':
+        q.is_full()
+    elif operation == 'm':
+        q.is_empty()
+    elif operation == 'e':
+        value = int(input("Enter the value to enqueue: "))
+        q.enqueue(value)
+    elif operation == 'd':
+        q.dequeue()
+    print("Enjoy The Program :)")
