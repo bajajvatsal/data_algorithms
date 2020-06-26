9217156c59f9c61ac825f1599ce5827fd866c812
Vatsal Bajaj <53997634+bajajvatsal@users.noreply.github.com>
2020-06-26 22:33:36 +0530

    Array algorithms and queue algorithms
    Array algorithms are added and loopholes from queue are removed and it is running properly now

---
+++ array_algorithms.py
@@ -0,0 +1,153 @@
+from list_creation import unordered_array
+
+
+class InvalidLimitError(Exception):
+    pass
+
+
+class ArrayOverflowError(Exception):
+    pass
+
+
+class ArrayIndexOutOfRangeError(Exception):
+    pass
+
+
+class ArrayValueNotFoundError(Exception):
+    pass
+
+
+class ArrayValueDeletionError(Exception):
+    pass
+
+
+class Array:
+    def __init__(self):
+        self.array = unordered_array(len_array)
+        self.limit = limit
+        self.length = len(self.array)
+
+    def insert_end(self, value):
+
+        if self.length == self.limit:
+            print("")
+            raise ArrayOverflowError("Array Overflow")
+        else:
+            self.array.append(value)
+            print(self.array)
+
+    def insert_front(self, value):
+        if self.length == self.limit:
+            raise ArrayOverflowError("Array Overflow")
+        else:
+            i = 1
+            while i > self.length:
+                self.array[i] = self.array[i - 1]
+                i = +1
+            self.array[0] = value
+            print(self.array)
+
+    def insert_local(self, index, value):
+        if self.length == self.limit:
+            raise ArrayOverflowError("Array Overflow")
+        elif index > self.length - 1:
+            raise ArrayIndexOutOfRangeError("Array Index Out of Range")
+        else:
+            i = index
+            while i < index:
+                self.array[i] = self.array[i + 1]
+                i = -1
+            self.array[index] = value
+            print(self.array)
+
+    def insert_after_given_value(self, search, value):
+
+        if self.length == self.limit:
+            raise ArrayOverflowError("Array Overflow")
+        else:
+            i = 0
+            while i < self.length:
+                if self.array[i] == search:
+                    break
+                else:
+                    i = +1
+                if i == self.length:
+                    print("no search value found no insertion")
+                else:
+                    j = self.length - 1
+                    while j > i:
+                        self.array[j + 1] = self.array[j]
+                        j = j - 1
+                        self.array[i + 1] = value
+                        self.length = +1
+        print(self.array)
+
+    def remove_end(self):
+        if self.length == 0:
+            raise ArrayValueDeletionError("Deletion not possible no elements found")
+        else:
+            self.array.remove(self.length)
+            print(self.array)
+
+    def remove_front(self):
+        if self.length == 0:
+            raise ArrayValueDeletionError("Deletion not possible no elements found")
+        else:
+            self.array.remove(self.array[0])
+            print(self.array)
+
+    def remove_at_index(self, index):
+
+        if self.length == 0:
+            raise ArrayValueDeletionError("Deletion not possible no elements found")
+        elif index > self.length - 1:
+            raise ArrayIndexOutOfRangeError("Array Index Out of Range")
+        else:
+            self.array.remove(self.array[index])
+        print(self.array)
+
+
+if __name__ == '__main__':
+    len_array = int(input("No of elements in the array: "))
+    limit = int(input("Limit of the array: "))
+    if len_array >= limit:
+        raise InvalidLimitError("Limit must be grater than length of the array")
+
+    a = Array()
+    print(f"Array to be operated on: {a.array}")
+    operation_type = input("For insertion press (i)\n"
+                           "For deletion press (d)\n"
+                           "Enter your response with given string in brackets: ")
+    if operation_type == "i":
+        operation = input("(1)Insert at end: \n"
+                          "(2)Insert at front: \n"
+                          "(3)Insert at given index: \n"
+                          "(4)insert after a given value: \n"
+                          "Enter your response with given number in brackets: ")
+        val = int(input("Enter the value to insert: "))
+        if operation == "1":
+            a.insert_end(val)
+        elif operation == "2":
+            a.insert_front(val)
+        elif operation == "3":
+            ind = int(input("Index for appending value: "))
+            if ind > len_array:
+                raise ArrayIndexOutOfRangeError
+            a.insert_local(val, ind)
+        elif operation == "4":
+            ind = int(input("After which value to append: "))
+            if ind not in a.array:
+                raise ArrayValueNotFoundError
+            a.insert_after_given_value(ind, val)
+    elif operation_type == "d":
+        operation = input("(5)delete at end: \n"
+                          "(6)delete at front: \n"
+                          "(7)Delete at given index: \n"
+                          "Enter your response with given number in brackets: ")
+        if operation == "5":
+            a.remove_end()
+        elif operation == "6":
+            a.remove_front()
+        elif operation == "7":
+            ind = int(input("Index to delete the element: "))
+            a.remove_at_index(ind)
---
+++ queue_algorithms.py
@@ -0,0 +1,66 @@
+from list_creation import unordered_array  # list of numbers of desired length randomly ordered no repetition
+
+
+class QueueOverflow(Exception):
+    pass
+
+
+class QueueUnderflow(Exception):
+    pass
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = unordered_array(length_array)
+        self.limit = limit_array
+        self.length = len(self.queue)
+        self.front = self.length-1
+        self.rear = 0
+
+    def is_full(self):
+        if self.front >= self.limit:
+            raise QueueOverflow("Queue Overflow")
+        else:
+            print("Space available for operations :)")
+
+    def is_empty(self):
+        if self.length == self.rear:
+            raise QueueUnderflow("Queue Underflow! No operations can be done")
+        else:
+            print("Space available for operations")
+
+    def enqueue(self, val):
+        if self.front == self.limit:
+            raise QueueOverflow("Queue Overflow! No operations can be done")
+        else:
+            self.queue.insert(0, val)
+            print(f"Enqueue : {self.queue}")
+
+    def dequeue(self):
+        if self.front == self.rear:
+            raise QueueUnderflow("Queue Underflow! No operations can be done")
+        else:
+            print(f"Element to be popped out is: {self.queue[self.front]}")
+            self.queue.pop(self.length - 1)
+            print(f"Dequeue : {self.queue}")
+
+
+if __name__ == '__main__':
+    length_array = int(input("Length of queue: "))
+    limit_array = int(input("Limit of queue: "))
+    if length_array >= limit_array:
+        raise QueueOverflow("Limit must be grater than length of the array! Re-run the program")
+    operation = input("which operation to do: (f)if_full, (m)is_empty, (e)enqueue, (d)dequeue: ")
+    q = Queue()
+    print(f"Queue to be operated on: {q.queue}")
+    if operation == 'f':
+        q.is_full()
+    elif operation == 'm':
+        q.is_empty()
+    elif operation == 'e':
+        value = int(input("Enter the value to enqueue: "))
+        q.enqueue(value)
+    elif operation == 'd':
+        q.dequeue()
+    print("Enjoy The Program :)")
